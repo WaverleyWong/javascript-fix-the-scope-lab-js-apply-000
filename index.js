86606107ca8b36f869359d572ca5a46ef03960e5
Waverley Wong <waverleybrontewong@gmail.com>
2018-11-05 21:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var animal = 'dog'
+
 
 function myAnimal() {
   return animal
+  var animal = 'dog'
 }
 
 function yourAnimal() {
@@ -9,13 +10,16 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+  var animal = 'cat'
 }
 
 function add2(n) {
-  return n + two
+  const two = 2
+  
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
@@ -26,4 +30,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
